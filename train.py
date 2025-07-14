82e46d4993eb41a7e7d85ca7a296e77619ec32c5
Kanupriya Pande <kpande@lbl.gov>
2025-07-14 13:05:15 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
 from torch.utils.data import DataLoader
-from dataset import PairedPETDataset
+from dataset import PETDataset
 from model import PET2PETModule
 import pytorch_lightning as pl
 from glob import glob
@@ -11,8 +11,8 @@ tracer2_train = glob("data/train/tracer2/*.npy")
 tracer1_val = glob("data/val/tracer1/*.npy")
 tracer2_val = glob("data/val/tracer2/*.npy")
 
-train_dataset = PairedPETDataset(tracer1_train, tracer2_train)
-val_dataset = PairedPETDataset(tracer1_val, tracer2_val)
+train_dataset = PETDataset(tracer1_train, tracer2_train)
+val_dataset = PETDataset(tracer1_val, tracer2_val)
 
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
 val_loader = DataLoader(val_dataset, batch_size=8)
